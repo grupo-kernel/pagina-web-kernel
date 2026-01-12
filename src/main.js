a10dbed03de58434a10b739acda311f1039661cf
JoelZhub <joelalbertobenitezvarela28@gmail.com>
2026-01-12 14:21:16 -0400

    colocacion del ico de la pagina, eliminacionde package-lock.json redudante, favor leer los comentarios del main

--- src/main.js
+++ src/main.js
@@ -10,3 +10,9 @@ navBar.innerHTML = createNavBar();
 
 initNavBar(navBar, header);
 
+
+//aqui inicializas todo los componentes que utilizaras, el contenido que cambiara es el del main
+//recuerda que el header. navBar y el footer son fijos, aun no he creado la logica de navegacion
+// pero ira en la carpeta router, ahi se definen las rutas y eso y los cambios entre componentes
+//pages, en el navBar me falta colocar los data-link para la navegacion, los otros cambios del footer
+// la page home
