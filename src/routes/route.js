5959a0316c41927c40b95e8d1ab581dc59c9e6b0
Angel Mosquea <mosqueaangel07@gmail.com>
2026-02-03 12:23:55 -0400

    feat: crear layout, modificar ruta, assets y components

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,22 @@
 import { CreatePageHome } from "../pages/Home.js";
 import { CreatePageQuienesSomos } from "../pages/QuienesSomos.js";
+import { setMainLayout } from "../layout/mainLayout.js";
 //colocar aqui las diversas paginas que se creen, ademas de agregar en navBar(componnete) el data-route correspondiente
 
 
 //agregar aqui la ruta correspondiente 
 const routes = {
-    home : CreatePageHome,
-    quienesSomos : CreatePageQuienesSomos
+    home: { page: CreatePageHome, layout: "default" },
+    quienesSomos: { page: CreatePageQuienesSomos, layout: "full" }
 };
 
-export function navigate (route){
-        const content = document.querySelector('main');
-        content.innerHTML  = "";
-        const page = routes[route];
-        if(!page) return;
-        return content.appendChild(page());
+export function navigate(route) {
+    const content = document.querySelector('main');
+    content.innerHTML = "";
+    const config = routes[route];
+    if (!config) return;
+
+    setMainLayout(config.layout);
+
+    return content.appendChild(config.page());
 }
