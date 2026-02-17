0a404362342cd555de5b610dba17bf4dd4dba6cb
Joel <joelalbertobenitezvarela28@gmail.com>
2026-02-16 22:45:54 -0400

    fead: ajuste del home en el md 768 pixeles

--- src/components/Home/CreateHome.js
+++ src/components/Home/CreateHome.js
@@ -3,8 +3,17 @@ import { CreateSliderComponentHome } from "./SliderComponent.js";
 
 export function CreateHome() {
         const section = document.createElement('section');
-        section.classList.add('relative', 'w-full', 'flex',  'flex-col', 'lg:flex-row');
+ section.classList.add(
+    "relative",
+    "w-full",
+    "flex",
+    "flex-col",
+    "lg:flex-row",
+    "md:mb-50",
+    "lg:mb-0",
+    "lg:pb-0"
 
+  );
         section.innerHTML = `${CreateCardFirstHome()}`;
 
         const  sliderComponete = document.createElement('div');
