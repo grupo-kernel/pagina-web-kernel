0cc55f0789776840d00748813a81cf7a2d82012a
Joel <joelalbertobenitezvarela28@gmail.com>
2026-02-18 05:37:41 -0400

    fead: ajuste en contacto

--- src/components/Contacto/CreateContact.js
+++ src/components/Contacto/CreateContact.js
@@ -2,7 +2,7 @@ import { contactForm } from "./ContactForm";
 
 export function createContact() {
     const section = document.createElement('section');
-    section.classList.add('w-full', 'tabletBig:w-7xl', 'xl:w-full');
+    section.classList.add('w-full', 'tabletBig:w-7xl');
     section.innerHTML = `
     ${contactForm()}
     `;
