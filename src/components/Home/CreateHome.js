2b112ea36993ecab8c6f80abf3ff46b3158488fa
Grupo.kernel <mleonardo@itla.edu.do>
2026-06-21 16:55:28 -0400

    Update CreateHome.js

--- src/components/Home/CreateHome.js
+++ src/components/Home/CreateHome.js
@@ -2,23 +2,23 @@ import { CreateCardFirstHome } from "./CardFirst.js";
 import { CreateSliderComponentHome } from "./SliderComponent.js";
 
 export function CreateHome() {
-       const section = document.createElement('section');
-       section.classList.add(
-              "relative", "w-full", "flex", "flex-col", "m-auto",
-              "tabletBig:flex-row", "md:mb-50",
-              "tabletBig:w-7xl", "tabletBig:px-20", 
-              "xl:mb-0", "xl:pb-0", "xl:w-full", "xl:px-0",
-              "2xl:w-450", "2xl:h-187.5"  
-              
-       );
-       section.innerHTML = `${CreateCardFirstHome()}`;
+    const section = document.createElement("section");
 
-       const sliderComponete = document.createElement('div');
-       sliderComponete.appendChild(CreateSliderComponentHome());
+    section.className = `
+        relative w-full max-w-7xl mx-auto px-4 py-10
+        flex flex-col tabletBig:flex-row
+        items-stretch justify-center
+        gap-0
+        font-sans
+        tabletBig:px-20
+        xl:px-0
+        2xl:max-w-[1800px]
+    `;
 
+    section.innerHTML = `${CreateCardFirstHome()}`;
 
-       section.appendChild(sliderComponete.firstElementChild);
+    const sliderComponent = CreateSliderComponentHome();
+    section.appendChild(sliderComponent);
 
-       return section;
-
+    return section;
 }
