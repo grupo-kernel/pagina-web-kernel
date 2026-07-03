6a47157918a1e26ff8f1386cf5e692bc77706c80
Grupo.kernel <mleonardo@itla.edu.do>
2026-07-03 11:25:45 -0400

    Update NewsGrid.js

--- src/components/Noticias/NewsGrid.js
+++ src/components/Noticias/NewsGrid.js
@@ -1,15 +1,21 @@
 import { newsCard } from "./NewsCard.js";
 
 export function newsGrid(newsList = []) {
-    const cards = newsList.map(news => newsCard(news)).join("");
+    const cards = newsList
+        .map(news => newsCard(news))
+        .join("");
 
     const cardGrid = `
-    <section role="news-grid" class="sm:px-10 lg:px-20 p-8">
-       <div class="max-w-7xl mx-auto px-8
-                grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-         ${cards}
-        </div>
-    </section>
+        <section aria-label="Listado de noticias" class="w-full py-10">
+            <div class="
+                max-w-7xl mx-auto px-6 md:px-8
+                grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3
+                gap-8
+            ">
+                ${cards}
+            </div>
+        </section>
     `;
+
     return cardGrid;
 }
