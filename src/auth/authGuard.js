e4ccf4672852fa39cc2d6f45023d312fad715870
Grupo.kernel <mleonardo@itla.edu.do>
2026-07-19 23:43:35 -0400

    Add authentication guard

--- src/auth/authGuard.js
+++ src/auth/authGuard.js
@@ -1,1 +1,14 @@
+import { auth, observarSesion } from "./authService.js";
 
+export function esperarAutenticacion() {
+  return new Promise((resolve) => {
+    const unsubscribe = observarSesion((user) => {
+      unsubscribe();
+      resolve(user);
+    });
+  });
+}
+
+export function usuarioActual() {
+  return auth.currentUser;
+}
