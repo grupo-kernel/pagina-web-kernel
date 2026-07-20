9dc81203ab8f362817f5626ab36eae694f4ab2d8
Grupo.kernel <mleonardo@itla.edu.do>
2026-07-19 23:38:17 -0400

    Add Firebase configuration

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBcz_uLXoV9R_fv9Li_K4szQjhRZ16ELZs",
+  authDomain: "kernel-laboratorio.firebaseapp.com",
+  projectId: "kernel-laboratorio",
+  storageBucket: "kernel-laboratorio.firebasestorage.app",
+  messagingSenderId: "1079989513841",
+  appId: "1:1079989513841:web:f5f588c60b2254531bf5d0"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
