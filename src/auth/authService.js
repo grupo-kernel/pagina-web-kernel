a9ee4f163575a2633cc5314808e4ceb53d7135f7
Grupo.kernel <mleonardo@itla.edu.do>
2026-07-19 23:42:12 -0400

    Add authentication service

--- src/auth/authService.js
+++ src/auth/authService.js
@@ -1,1 +1,21 @@
+import { auth } from "../firebase/firebaseConfig.js";
 
+import {
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+export async function iniciarSesion(email, password) {
+  return await signInWithEmailAndPassword(auth, email, password);
+}
+
+export async function cerrarSesion() {
+  return await signOut(auth);
+}
+
+export function observarSesion(callback) {
+  return onAuthStateChanged(auth, callback);
+}
+
+export { auth };
