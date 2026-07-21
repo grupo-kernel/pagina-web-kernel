19c61937c43f85ef56b28d06a9da6f009b4ccac4
Grupo.kernel <mleonardo@itla.edu.do>
2026-07-21 09:48:53 -0400

    Update header.js

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,7 +1,7 @@
 import kernel from "../../assets/ElKernel.png";
 
 export function createHeader() {
-    const wrapper = `<div class="flex items-center px-2.5   w-full  ">
+    const wrapper = `<div class="flex items-center px-0 w-full">
         <img  class="object-fill " width="80px" heigth="80px" src="${kernel}" alt="Grupo de investigacion dominicano" _blank title="El kernel">
         <span class="text-3xl xl:text-[42px] py-2.5 font-medium whitespace-nowrap">El Kernel</span>
         </div>
