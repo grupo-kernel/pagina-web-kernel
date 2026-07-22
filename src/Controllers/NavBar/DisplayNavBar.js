dd0ba8771e2423496d69f38d81308e7cf2efdea4
Grupo.kernel <mleonardo@itla.edu.do>
2026-07-22 10:21:47 -0400

    Hacer segura la apertura del menú móvil

--- src/Controllers/NavBar/DisplayNavBar.js
+++ src/Controllers/NavBar/DisplayNavBar.js
@@ -1,9 +1,9 @@
-export function DisplayNavBar(Iconmenu, navBar, ){
-         Iconmenu.addEventListener("click", ()=> {
-                    navBar.classList.replace('hidden', 'block');      
-                    document.body.style.overflow = 'hidden'; 
-                    
-                  
-                    
-            });
+export function DisplayNavBar(iconoMenu, navBar) {
+    if (!iconoMenu || !navBar) return;
+
+    iconoMenu.addEventListener("click", () => {
+        navBar.classList.remove("hidden");
+        navBar.classList.add("block");
+        document.body.style.overflow = "hidden";
+    });
 }
