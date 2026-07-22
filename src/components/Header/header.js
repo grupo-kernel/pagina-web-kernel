b13d3d5131345d47fa02f38ccf8b73130ef80373
Grupo.kernel <mleonardo@itla.edu.do>
2026-07-22 07:14:42 -0400

    Restore optimized header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,12 +1,21 @@
-import kernel from "../../assets/ElKernel.png";
+import kernel from "../../../favicon.ico";
 
 export function createHeader() {
     const wrapper = `<div class="flex items-center px-0 w-full">
-        <img  class="object-fill " width="80px" heigth="80px" src="${kernel}" alt="Grupo de investigacion dominicano" _blank title="El kernel">
+        <img
+            class="h-16 w-16 object-contain"
+            width="64"
+            height="64"
+            src="${kernel}"
+            alt="Logo del Grupo de Investigación Kernel"
+            title="El Kernel"
+            loading="eager"
+            fetchpriority="high"
+        >
         <span class="text-3xl xl:text-[42px] py-2.5 font-medium whitespace-nowrap">El Kernel</span>
         </div>
         <div class="flex items-center lg:hidden">
-            <i data-action="open-menu" class=" text-5xl cursor-pointer bx bx-menu"></i> 
+            <i data-action="open-menu" class="text-5xl cursor-pointer bx bx-menu"></i>
         </div>`;
     return wrapper;
 }
