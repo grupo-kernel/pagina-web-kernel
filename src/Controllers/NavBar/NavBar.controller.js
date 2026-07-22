e5daf46c15965f54fcea608a4c29884415f86c13
Grupo.kernel <mleonardo@itla.edu.do>
2026-07-22 10:05:28 -0400

    Inicializar submenús una sola vez

--- src/Controllers/NavBar/NavBar.controller.js
+++ src/Controllers/NavBar/NavBar.controller.js
@@ -1,33 +1,16 @@
-
 import { DisplaySubMenu } from "./DisplaySubMenu.js";
 import { DisplayNavBar } from "./DisplayNavBar.js";
 import { closeNavBar } from "./CloseNavBar.js";
-import { navigate } from "../../routes/route.js";
+import { navigate, routerInit } from "../../routes/route.js";
 import { onNavigate } from "./onNavigate.js";
-import { routerInit } from "../../routes/route.js";
 
-export function initNavBar(nav, header){
+export function initNavBar(nav, header) {
     onNavigate(nav, navigate);
-    routerInit();
-
-    const mq = window.matchMedia('(max-width: 768px)');
-    let subMenuInitialized = false;
-
-    function handleBreakpoint(e) {
-        if (e.matches && !subMenuInitialized) {
-        DisplaySubMenu(nav);
-        subMenuInitialized = true;
-        }
-    }
-
-  mq.addEventListener('change', handleBreakpoint);
-  handleBreakpoint(mq);
-
     DisplaySubMenu(nav);
-    const icon = header.querySelector('[data-action ="open-menu"]');
-    DisplayNavBar(icon, nav);
-    closeNavBar(nav);
 
-    
-   
+    const iconoMenu = header.querySelector('[data-action="open-menu"]');
+    if (iconoMenu) DisplayNavBar(iconoMenu, nav);
+
+    closeNavBar(nav);
+    routerInit();
 }
