c4fe03b9a71e19884d92c12ff17557a1cffcb522
Grupo.kernel <mleonardo@itla.edu.do>
2026-07-24 21:18:47 -0400

    Centralizar métricas institucionales del grupo

---
+++ src/data/metricasInstitucionales.js
@@ -0,0 +1,8 @@
+export const METRICAS_INSTITUCIONALES = Object.freeze({
+    integrantes: 9,
+    articulosAcumulados: 572,
+    articulosAcumuladosTexto: "572+",
+    etiquetaArticulos: "Artículos científicos acumulados",
+    descripcionArticulos: "Producción de los 9 integrantes a lo largo de sus trayectorias académicas",
+    notaArticulos: "Cota mínima basada en artículos de revista declarados en perfiles institucionales y registros científicos verificados. La suma representa la producción individual acumulada; las coautorías compartidas pueden formar parte de más de una trayectoria."
+});
