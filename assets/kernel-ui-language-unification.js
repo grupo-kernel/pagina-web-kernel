ab4aa87bfd43585c3264c865a0f22de43d53cf65
Grupo.kernel <mleonardo@itla.edu.do>
2026-07-26 19:22:01 -0400

    Add unified interface and English layer

---
+++ assets/kernel-ui-language-unification.js
@@ -0,0 +1,9 @@
+(() => {
+  "use strict";
+
+  if (window.KernelUILanguageUnification) return;
+
+  const STYLE_ID = "kernel-ui-language-unification-styles";
+  const DATA_URL = "./core/data/researchers.v2.json";
+  const ENTRIES = __PLACEHOLDER__;
+})();
